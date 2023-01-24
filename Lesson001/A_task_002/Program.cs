e513b9e8c420d787ec095b0b5eb4a7b396fab43d
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-01-24 22:02:00 +0800

    Предварительное решение для 2 задачи для д. з. первого семинара!

---
+++ Lesson001/A_task_002/Program.cs
@@ -0,0 +1,105 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число a:");
+string? A_number_a = Console.ReadLine();
+if (A_number_a == null)
+{
+    // ...
+}
+
+int a = Convert.ToInt32(A_number_a);
+// int a = 5;
+
+Console.WriteLine("Введите число b:");
+string? A_number_b = Console.ReadLine();
+if (A_number_b == null)
+{
+    // ...
+}
+
+int b = Convert.ToInt32(A_number_b); 
+// int b = 7;
+
+if (a > b)
+{
+    int max = a;
+    Console.WriteLine("a - больше");
+    Console.WriteLine("MAX = " + max);
+}
+
+else
+{
+    int max = b;
+    Console.WriteLine("b - больше");
+    Console.WriteLine("MAX = " + max);
+}
+Console.Clear();
+
+Console.WriteLine("Введите число a:");
+string? A_number_a = Console.ReadLine();
+if (A_number_a == null)
+{
+    // ...
+}
+
+int a = Convert.ToInt32(A_number_a);
+// int a = 5;
+
+Console.WriteLine("Введите число b:");
+string? A_number_b = Console.ReadLine();
+if (A_number_b == null)
+{
+    // ...
+}
+
+int b = Convert.ToInt32(A_number_b); 
+// int b = 7;
+
+if (a > b)
+{
+    int max = a;
+    Console.WriteLine("a - больше");
+    Console.WriteLine("MAX = " + max);
+}
+
+else
+{
+    int max = b;
+    Console.WriteLine("b - больше");
+    Console.WriteLine("MAX = " + max);
+}
+Console.Clear();
+
+Console.WriteLine("Введите число a:");
+string? A_number_a = Console.ReadLine();
+if (A_number_a == null)
+{
+    // ...
+}
+
+int a = Convert.ToInt32(A_number_a);
+// int a = 5;
+
+Console.WriteLine("Введите число b:");
+string? A_number_b = Console.ReadLine();
+if (A_number_b == null)
+{
+    // ...
+}
+
+int b = Convert.ToInt32(A_number_b); 
+// int b = 7;
+
+if (a > b)
+{
+    int max = a;
+    Console.WriteLine("a - больше");
+    Console.WriteLine("MAX = " + max);
+}
+
+else
+{
+    int max = b;
+    Console.WriteLine("b - больше");
+    Console.WriteLine("MAX = " + max);
+}
