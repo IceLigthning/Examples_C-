4c732290f41f784bf2e3d02644f7b10a07657858
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-02-03 22:12:35 +0800

    Has read a program for task 2!

--- Lesson001/A_task_002/Program.cs
+++ Lesson001/A_task_002/Program.cs
@@ -10,106 +10,24 @@ a = -9 b = -3 -> max = -3
 
 Console.Clear();
 
-Console.WriteLine("Введите число a:");
-string? A_number_a = Console.ReadLine();
-if (A_number_a == null)
+Console.Write("Enter a first namber: ");
+string inputNumber_a = Console.ReadLine();
+int number_a = Convert.ToInt32(inputNumber_a);
+
+Console.Write("Enter a second namber: ");
+string inputNumber_b = Console.ReadLine();
+int number_b = Convert.ToInt32(inputNumber_b);
+
+Console.WriteLine();
+
+if (number_a > number_b)
 {
-    // ...
-}
-
-int a = Convert.ToInt32(A_number_a);
-// int a = 5;
-
-Console.WriteLine("Введите число b:");
-string? A_number_b = Console.ReadLine();
-if (A_number_b == null)
-{
-    // ...
-}
-
-int b = Convert.ToInt32(A_number_b); 
-// int b = 7;
-
-if (a > b)
-{
-    int max = a;
-    Console.WriteLine("a - больше");
-    Console.WriteLine("MAX = " + max);
+    int max = number_a;
+    Console.WriteLine($"Max = {max}");
 }
 
 else
 {
-    int max = b;
-    Console.WriteLine("b - больше");
-    Console.WriteLine("MAX = " + max);
-}
-Console.Clear();
-
-Console.WriteLine("Введите число a:");
-string? A_number_a = Console.ReadLine();
-if (A_number_a == null)
-{
-    // ...
-}
-
-int a = Convert.ToInt32(A_number_a);
-// int a = 5;
-
-Console.WriteLine("Введите число b:");
-string? A_number_b = Console.ReadLine();
-if (A_number_b == null)
-{
-    // ...
-}
-
-int b = Convert.ToInt32(A_number_b); 
-// int b = 7;
-
-if (a > b)
-{
-    int max = a;
-    Console.WriteLine("a - больше");
-    Console.WriteLine("MAX = " + max);
-}
-
-else
-{
-    int max = b;
-    Console.WriteLine("b - больше");
-    Console.WriteLine("MAX = " + max);
-}
-Console.Clear();
-
-Console.WriteLine("Введите число a:");
-string? A_number_a = Console.ReadLine();
-if (A_number_a == null)
-{
-    // ...
-}
-
-int a = Convert.ToInt32(A_number_a);
-// int a = 5;
-
-Console.WriteLine("Введите число b:");
-string? A_number_b = Console.ReadLine();
-if (A_number_b == null)
-{
-    // ...
-}
-
-int b = Convert.ToInt32(A_number_b); 
-// int b = 7;
-
-if (a > b)
-{
-    int max = a;
-    Console.WriteLine("a - больше");
-    Console.WriteLine("MAX = " + max);
-}
-
-else
-{
-    int max = b;
-    Console.WriteLine("b - больше");
-    Console.WriteLine("MAX = " + max);
+    int max = number_b;
+    Console.WriteLine($"Max = {max}");
 }
