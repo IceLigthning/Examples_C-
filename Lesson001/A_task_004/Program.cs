1dc9e3a00eedb52fe3147ecc137cfc46126ed861
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-02-04 15:42:01 +0800

    A program for task №4 has been created!

--- Lesson001/A_task_004/Program.cs
+++ Lesson001/A_task_004/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 4: Напишите программу, которая принимает 
+на вход три числа и выдаёт максимальное из этих 
+чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Clear();
+
+Console.Write("Enter fiest number: ");
+string inputFirstNumber = Console.ReadLine();
+int firstNumber = Convert.ToInt32(inputFirstNumber);
+
+Console.Write("Enter second number: ");
+string inputSecondNumber = Console.ReadLine();
+int secondNumber = Convert.ToInt32(inputSecondNumber);
+
+Console.Write("Enter third number: ");
+string inputThirdNumber = Console.ReadLine();
+int thirdNumber = Convert.ToInt32(inputThirdNumber);
+
+Console.WriteLine();
+
+if(firstNumber > secondNumber)
+{
+    int max = firstNumber;
+    if(max > thirdNumber)
+    {
+        Console.WriteLine($"Max = {max}");
+    }
+
+    else
+    {
+        max = thirdNumber;
+        Console.WriteLine($"Max = {max}");
+    }
+}
+else
+{
+    int max = secondNumber;
+    if(max > thirdNumber)
+{
+    Console.WriteLine($"Max = {max}");
+}
+else
+{
+    max = thirdNumber;
+    Console.WriteLine($"Max = {max}");
+}
+
+}
+
