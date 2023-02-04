8c2b3f52b8ef0cb55d68e8c2903a4ce49dce7488
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-02-04 15:57:14 +0800

    A program for task_006 has been writen!

--- Lesson001/A_task_006/Program.cs
+++ Lesson001/A_task_006/Program.cs
@@ -1,5 +1,33 @@
-﻿
-int a = Next(int 1, int 10);
+﻿/*
+Задача 6: Напишите программу, которая на 
+вход принимает число и выдаёт, является 
+ли число чётным (делится ли оно на два без 
+остатка).
 
-Console.Write(a);
+4 -> да
+-3 -> нет
+7 -> нет
+*/
 
+Console.Clear();
+
+Console.WriteLine("Is a number even?");
+
+Console.WriteLine();
+
+Console.Write("Enter a number: ");
+
+string inputNumber = Console.ReadLine();
+
+int number = Convert.ToInt32(inputNumber);
+
+Console.WriteLine();
+
+if(number%2 == 0)
+{
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Нет!");
+}
