956706186309b4618353fd866f97d1810c923194
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-02-08 22:39:29 +0800

    A programm for task_027 has writen!

---
+++ Lesson004/A_task_025/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход 
+два числа (A и B) и возводит число A в натуральную 
+степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+// Отчиста строки терминала.
+Console.Clear();
+
+// Получаем число из терминала.
+int GetNumber()
+{
+    int resultNumber = 0;
+    while(true)
+    {
+        
+        if (int.TryParse(Console.ReadLine(), out resultNumber))
+        {
+            break;
+        }
+        
+        else
+        
+        {
+            Console.WriteLine("A number's incorrect!");
+        }
+    }
+
+    return resultNumber;
+}
+
+// Получаем степенное число.
+int GetPowerNumber(int first, int second)
+{
+    int degree = 1;
+
+    for(int inter = 0;   inter < second; inter++)
+    {
+        degree = degree * first;
+    }
+
+    return degree;
+}
+
+Console.Write("Enter first number: ");
+int getFirstNumber = GetNumber();
+// Вводип число.
+
+Console.Write("Enter second number: ");
+int getSecondNumber = GetNumber();
+// Вводим стпень числа.
+
+int getLast = GetPowerNumber(getFirstNumber, getSecondNumber);
+
+Console.WriteLine("");
+Console.WriteLine($"The power number = {getLast} of number {getFirstNumber} with degree {getSecondNumber}.");
+
+
---
+++ Lesson004/A_task_027/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Задача 27: Напишите программу, которая 
+принимает на вход число и выдаёт сумму 
+цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+
+// Вводим число.
+int GetNumber()
+{
+    int resultNumber = 0;
+    Console.Write("Enter a number: ");
+
+    while(true)
+    {
+
+        if(int.TryParse(Console.ReadLine(), out resultNumber))
+        {
+            break;
+        }
+
+        else
+        {
+            Console.Write("The number is incorrect! Enter a correct number: ");
+        }
+    }
+    return resultNumber;
+}
+
+int aNumber = GetNumber();
+
+// Определяем сколько раз число делится на 10;
+int GetSizeNumber(int primeNumber)
+{
+    int count = 0;
+    while(primeNumber != 0)
+    {
+       primeNumber = primeNumber / 10;
+       count++;
+    }
+    return count;
+    
+}
+
+int aSizeNumber = GetSizeNumber(aNumber);
+
+// Определяем сумму цифр в числе.
+int GetSumm()
+{ 
+    int ten = 1;
+    int sum = 0;
+
+    for(int interation = aSizeNumber; interation > 0; interation = interation - 1)
+    {   
+        
+        for(int interat = 0; interat < interation; interat++)
+        {
+            ten = ten * 10;
+            break;
+        }
+
+        int unitNumber = (aNumber / (ten/10)) - (aNumber / ten) * 10;
+        
+        sum = sum + unitNumber;
+                       
+        Console.WriteLine($"inter {interation} ten {ten} unit {unitNumber}");
+
+    }
+
+    return sum;
+}
+
+Console.WriteLine(GetSumm());
