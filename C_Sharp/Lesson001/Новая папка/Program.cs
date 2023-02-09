726bbe58db13a70b085482b764fe8bab7dcfe0fd
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-02-09 22:20:26 +0800

    Создан файл макрдаун!

---
+++ C_Sharp/Lesson001/Новая папка/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход 
+принимает число (N), а на выходе показывает 
+все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+
+Console.WriteLine("Enter a number: ");
+
+string inputNumber = Console.ReadLine();
+int number = Convert.ToInt32(inputNumber);
+
+int i = 1;
+
+while(i < number)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+
+    i = i + 1;
+}
